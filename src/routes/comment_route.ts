283eec4d3611a25dc2b19f2218898d8df214245f
Oren <oren.hta@gmail.com>
2024-02-07 23:16:15 +0200

    remove unused routes

--- src/routes/comment_route.ts
+++ src/routes/comment_route.ts
@@ -3,31 +3,10 @@ const router = express.Router();
 import CommentController from "../controllers/comment_controller";
 import authMiddleware from "../common/auth_middleware";
 
-router.get("/", CommentController.get.bind(CommentController));
-
-router.get("/:id", CommentController.getById.bind(CommentController));
-
-router.get(
-  "/review/:id",
-  CommentController.getByReviewId.bind(CommentController)
-);
-
 router.post(
   "/",
   authMiddleware,
   CommentController.post.bind(CommentController)
 );
-
-// router.put(
-//   "/:id",
-//   authMiddleware,
-//   CommentController.putCommentById.bind(CommentController)
-// );
-
-// router.delete(
-//   "/:id",
-//   authMiddleware,
-//   CommentController.deleteCommentById.bind(CommentController)
-// );
 
 export default router;
--- src/routes/user_route.ts
+++ src/routes/user_route.ts
@@ -9,10 +9,6 @@ router.get(
   UserController.getConnected.bind(UserController)
 );
 
-router.get("/:id", authMiddleware, UserController.getById.bind(UserController));
-
-router.post("/", authMiddleware, UserController.post.bind(UserController));
-
 router.put("/", authMiddleware, UserController.putById.bind(UserController));
 
 export default router;
